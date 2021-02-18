fa7638eee0bdf94c69124b8438595086e7a577f0
Aleksandr Maliarevskii <luexe@yandex.ru>
2021-02-18 15:49:45 +0300

    changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(function() {
+$(function () {
     var videos = $(".main__video");
 
-    videos.on("click", function() {
+    videos.on("click", function () {
         var elm = $(this),
             conts = elm.contents(),
             le = conts.length,
@@ -16,7 +16,7 @@ $(function() {
     });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('.slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -27,3 +27,62 @@ $(document).ready(function() {
         nextArrow: $('.slider-arrows__next')
     });
 });
+
+
+function toggleMenu(event) {
+    if (event.stopPropagation) {
+        event.stopPropagation();
+    }
+    else if (window.event) {
+        window.event.cancelBubble = true;
+    }
+    const asideMenu = document.querySelector('#aside');
+    const body = document.querySelector('body');
+    asideMenu.classList.toggle('aside--shown');
+
+    if (asideMenu.classList.contains('aside--shown')) {
+        asideMenu.style.transform = 'translate(0px)';
+        body.style.overflow = 'hidden';
+    } else {
+        asideMenu.style.transform = 'translate(-320px)';
+        body.style.overflow = 'auto';
+    }
+}
+
+function initMenu() {
+    const asideMenu = document.querySelector('#aside');
+    asideMenu.classList.remove('aside--shown');
+    asideMenu.style.transform = 'translate(-320px)';
+}
+
+function closeMenu() {
+    initMenu();
+    document.querySelector('body').style.overflow = 'auto';
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1023) {
+        initMenu();
+    }
+})
+
+window.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const aside = document.querySelector("#aside");
+    if (outsideClick(e, aside)) {
+        closeMenu()
+    }
+});
+
+
+function outsideClick(event, notelem) {
+    notelem = $(notelem);
+    var clickedOut = true, i, len = notelem.length;
+    for (i = 0; i < len; i++) {
+        if (event.target == notelem[i] || notelem[i].contains(event.target)) {
+            clickedOut = false;
+        }
+    }
+    if (clickedOut) return true;
+    else return false;
+}
